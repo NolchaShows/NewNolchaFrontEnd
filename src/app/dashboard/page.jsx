c1d7e64c7fdb390af338f70397506dc4fc864721
TayabAmir <amirtayyab8@gmail.com>
2025-08-26 10:20:59 +0500

    Dashboard Sidebar created

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import Sidebar from '@/components/dashboard/Sidebar'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default Page
