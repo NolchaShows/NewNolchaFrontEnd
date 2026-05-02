71789744e49ceb67663ffcc165cb26238746e782
Abdullah Fiaz <itsabdullahfiaz@gmail.com>
2026-05-02 21:08:45 +0500

    Update breakpoints in globals.css and refactor StyledHeading component
    - Changed breakpoint units in globals.css from pixels to rem for better compatibility with Tailwind's defaults.
    - Refactored StyledHeading component to simplify class names and enhance responsiveness across various screen sizes.
    - Improved overall styling consistency and maintainability in the application.

--- src/components/common/StyledHeading.jsx
+++ src/components/common/StyledHeading.jsx
@@ -10,7 +10,7 @@ const StyledHeading = ({
   size = 'large'
 }) => {
   return (
-    <h1 className={`${size == 'medium' ? 'text-[40px] lg:text-[70px] 2xl:text-[124px] leading-none' : size == 'small' ? 'text-[40px] lg:text-[56px] 2xl:text-[100px] leading-none' : 'text-[48px] lg:text-[82px] 2xl:text-[150px] leading-[57px] lg:leading-[96px] 2xl:leading-[180px]' } tracking-[-1.44px] lg:tracking-[-2.4px] 2xl:tracking-[-4.5px] text-center font-bold ${className}`}>
+    <h1 className={`text-[48px] lg:text-[82px] xl:text-[90px] 2xl:text-[110px] 3xl:text-[130px] leading-[57px] lg:leading-[96px] xl:leading-[112px] 2xl:leading-[128px] 3xl:leading-[128px]' tracking-[-1.44px] lg:tracking-[-2.4px] xl:tracking-[-3.2px] 2xl:tracking-[-3.8px] 3xl:tracking-[-4.8px] text-center font-bold ${className}`}>
       <span style={{ color: textColor }}>
         {firstPart}
       </span>{" "}
