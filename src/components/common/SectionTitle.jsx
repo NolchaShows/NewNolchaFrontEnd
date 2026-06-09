46c997223a312eb48042e2063c3065fca06f7e63
Abdullah Fiaz <itsabdullahfiaz@gmail.com>
2026-06-09 23:27:34 +0500

    Enhance SectionTitle and StyledHeading Components with Tone and Style Adjustments
    - Added a tone prop to SectionTitle for dynamic text color based on light or dark themes, improving visual adaptability.
    - Updated StyledHeading to adjust font sizes and leading for better readability and consistency, while also applying uppercase styling for emphasis.

--- src/components/common/SectionTitle.jsx
+++ src/components/common/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SectionTitle = ({ children, className = "", disableTitleSpacing = false }) => {
   return (
-    <h2 className={`${className} text-white text-h1`}>
+    <h2 className={`${className} text-white text-h1 uppercase`}>
       {children}
     </h2>
   );
--- src/components/common/StyledHeading.jsx
+++ src/components/common/StyledHeading.jsx
@@ -10,7 +10,7 @@ const StyledHeading = ({
   size = 'large'
 }) => {
   return (
-    <h1 className={`text-[48px] lg:text-[82px] xl:text-[90px] 2xl:text-[110px] 3xl:text-[130px] leading-[57px] lg:leading-[96px] xl:leading-[112px] 2xl:leading-[128px] 3xl:leading-[128px]' tracking-[-1.44px] lg:tracking-[-2.4px] xl:tracking-[-3.2px] 2xl:tracking-[-3.8px] 3xl:tracking-[-4.8px] text-center font-bold ${className}`}>
+    <h1 className={`text-[42px] lg:text-[72px] xl:text-[80px] 2xl:text-[96px] 3xl:text-[112px] leading-[50px] lg:leading-[84px] xl:leading-[98px] 2xl:leading-[112px] 3xl:leading-[112px] tracking-[-1.2px] lg:tracking-[-2.1px] xl:tracking-[-2.8px] 2xl:tracking-[-3.3px] 3xl:tracking-[-4.1px] text-center uppercase ${className}`}>
       <span style={{ color: textColor }}>
         {firstPart}
       </span>{" "}
